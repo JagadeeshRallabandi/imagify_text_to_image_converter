b512e2bac0025f16feb0edacd8c2316795b45f03
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2025-06-25 12:53:19 +0530

    HomePage_done_2_00

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+import Description from '../components/Description'
+import Testimonial from '../components/Testimonial'
+import GenerateBtn from '../components/GenerateBtn'
 
 const Home = () => {
   return (
-    <div></div>
+    <div>
+      <Header/>
+      <Steps/>
+      <Description/>
+      <Testimonial/>
+      <GenerateBtn/>
+    </div>
   )
 }
 
