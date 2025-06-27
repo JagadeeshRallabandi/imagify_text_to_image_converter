6a5ecba84ddfea1926f35b3b1049293ed8152e2f
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2025-06-27 09:22:51 +0530

    till_razor_pay

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Result from './pages/Result'
@@ -15,6 +16,7 @@ const App = () => {
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 
     min-h-screen bg-gradient-to-b from-teal-50
     to-orange-50'>
+    <ToastContainer position='bottom-right'/>
     <Navbar/>
     {showLogin && <Login/>}
     <Routes>
